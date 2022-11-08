6e62faf3df3d7f9a1d6c0a4e7c167402c1442aad
staric1097 <matveev.sds@yandex.ru>
2022-11-08 19:23:50 +0300

    Add project files.

---
+++ Quiz/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Quiz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(100, 25);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.label3);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.pictureBox1);
+            this.splitContainer1.Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel1_Paint);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.button3);
+            this.splitContainer1.Panel2.Controls.Add(this.button2);
+            this.splitContainer1.Panel2.Controls.Add(this.button1);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 500);
+            this.splitContainer1.SplitterDistance = 350;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(706, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 35);
+            this.label3.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(3, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 44);
+            this.label2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(3, 314);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(100, 10);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(600, 300);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(533, 0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(266, 150);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(266, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(267, 150);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(266, 150);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(982, 553);
+            this.Controls.Add(this.splitContainer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Quiz";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Quiz/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Quiz
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button2.Text = "Нажмите, чтобы начать";
+            button1.Text = "";
+            button3.Text = "";
+            label1.Text = "" ;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void splitContainer2_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void splitContainer2_SplitterMoved(object sender, SplitterEventArgs e)
+        {
+
+        }
+
+        private void splitContainer2_Panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+                var curQ = Question.CreateQ(Question.QNumber);
+                curQ.startNewQ(label1, pictureBox1, button1, button2, button3);
+            if (Question.QNumber != 1)
+            {
+                var curQPr = Question.CreateQ(Question.QNumber - 1);
+                if (curQPr.rightAnswer == 1)
+                {
+                    Question.rightAnsCount++;
+                    label3.Text = "right!";
+                }
+                else
+                {
+                    label3.Text = "wrong!";
+                }
+                label2.Text = $"{Question.rightAnsCount}/{Question.QNumber-1}";
+                Question.QNumber++;
+                if (Question.QNumber > 5)
+                {
+                    MessageBox.Show($"Количество правильных ответов:{Question.rightAnsCount} / {Question.QNumber-2}");
+                    button1.Enabled = false;
+                    button2.Enabled = false;
+                    button3.Enabled = false;
+                }
+            }
+            else Question.QNumber++;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+                var curQ = Question.CreateQ(Question.QNumber);
+                curQ.startNewQ(label1, pictureBox1, button1, button2, button3);
+            if (Question.QNumber != 1)
+            {
+                var curQPr = Question.CreateQ(Question.QNumber-1);
+                if (curQPr.rightAnswer == 2)
+                {
+                    Question.rightAnsCount++;
+                    label3.Text = "right!";
+                }
+                else
+                {
+                    label3.Text = "wrong!";
+                }
+                label2.Text = $"{Question.rightAnsCount}/{Question.QNumber-1}";
+                Question.QNumber++;
+                if (Question.QNumber > 5)
+                {
+                    MessageBox.Show($"Количество правильных ответов:{Question.rightAnsCount} / {Question.QNumber-2}");
+                    button1.Enabled = false;
+                    button2.Enabled = false;
+                    button3.Enabled = false;
+                }
+            }
+            else Question.QNumber++;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+                var curQ = Question.CreateQ(Question.QNumber);
+                curQ.startNewQ(label1, pictureBox1, button1, button2, button3);
+            if (Question.QNumber != 1)
+            {
+                var curQPr = Question.CreateQ(Question.QNumber - 1);
+                if (curQPr.rightAnswer == 3)
+                {
+                    Question.rightAnsCount++;
+                    label3.Text = "right!";
+                }
+                else
+                {
+                    label3.Text = "wrong!";
+                }
+                label2.Text = $"{Question.rightAnsCount}/{Question.QNumber-1}";
+                Question.QNumber++;
+                if (Question.QNumber > 5)
+                {
+                    MessageBox.Show($"Количество правильных ответов:{Question.rightAnsCount} / {Question.QNumber-2}");
+                    button1.Enabled = false;
+                    button2.Enabled = false;
+                    button3.Enabled = false;
+                }
+            }
+            else Question.QNumber++;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    class Question
+    {
+        static public int rightAnsCount=0;
+        static public int QNumber = 1;
+        private string[] Btext;
+        public int rightAnswer;
+        private string pictureName;
+        private string task;
+        private int currentQ;
+        public Question(string[] Btext, int rightAnswer, string pictureName, string task, int currentQ)
+        {
+            this.Btext = Btext;
+            this.rightAnswer = rightAnswer;
+            this.pictureName = pictureName;
+            this.task = task;
+            this.currentQ = currentQ;
+        }
+        public void startNewQ(Label label, PictureBox pictureBox, Button b1, Button b2, Button b3)
+        {
+            label.Text = this.task;
+            pictureBox.Image = Image.FromFile($"{Environment.CurrentDirectory}\\Quiz{currentQ}.jpg");
+            b1.Text = this.Btext[0];
+            b2.Text = this.Btext[1];
+            b3.Text = this.Btext[2];
+        }
+        static public Question CreateQ(int Qnumber)
+        {
+            switch (Qnumber)
+            {
+                case 1:
+                    string[] Btext1 = { "Для создания программ", "Для перевода программ в машинные коды", "Для обеспечения бесперебойной работы программ" };
+                    var firstQ = new Question(Btext1, 2, "", "1.Для чего используют программы-ассемблеры?", 1);
+                    return firstQ;
+                case 2:
+                    string[] Btext2 = { "К языкам среднего уровня", "К языкам высокого уровня", "К языкам низкого уровня" };
+                    var secondQ = new Question(Btext2, 3, "", "2.К какому уровню языков относятся языки ассемблера?", 2);
+                    return secondQ;
+                case 3:
+                    string[] Btext3 = { "Программные средства для обеспечения бесперебойной работы существующих программ", "Программные средства для создания и отладки новых программ", "Программные средства для перевода команд с естественного языка в машинные коды" };
+                    var thirdQ = new Question(Btext3, 2, "", "3.Что такое системы программирования?", 3);
+                    return thirdQ;
+                case 4:
+                    string[] Btext4 = { "Ада", "Паскаль", "Фортран" };
+                    var fourthQ = new Question(Btext4, 3, "", "4.Какой язык программирования, созданный в 1957 году, является одним из первых алгоритмических языков и до\n сих пор применяется для научных вычислений?", 4);
+                    return fourthQ;
+                default:
+                    string[] temp = { "", "", "" };
+                    return new Question(temp, 0, "", "", 5);
+            }
+        }
+    }
+}
